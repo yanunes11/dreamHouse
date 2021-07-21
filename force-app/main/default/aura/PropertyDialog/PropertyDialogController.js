d4ffad79683aebb241e9d865ed44a611cbfdc48d
Yan Nascimento <yannunes112@gmail.com>
2021-07-21 08:10:29 -0300

    end

--- force-app/main/default/aura/PropertyDialog/PropertyDialogController.js
+++ force-app/main/default/aura/PropertyDialog/PropertyDialogController.js
@@ -1,18 +1,25 @@
 ({
     doInit : function(component, event, helper) {
-        component.find("forceRecord").getNewRecord(
-            "Property__c",
-            null,
-            false,
-            $A.getCallback(function() {
-                var rec = component.get("v.propertyRecord");
-                var error = component.get("v.recordError");
-                if (error || (rec === null)) {
-                    console.log("Error initializing record template: " + error);
-                    return;
-                }
-            })
-        );
+        var recId = component.get("v.recordId");
+        if (recId) {
+            component.set("v.modalContext", "Edit");
+        }
+        if (!recId) {
+            component.find("forceRecord").getNewRecord(
+                "Property__c",
+                null,
+                false,
+                $A.getCallback(function() {
+                    var rec = component.get("v.propertyRecord");
+                    var error = component.get("v.recordError");
+                    if (error || (rec === null)) {
+                        console.log("Error initializing record template: " + error);
+                        return;
+                    }
+                })
+            );
+        }
+        
     },
     saveRecord : function(component, event, helper) {
         var propBeds = parseInt(component.find('propBeds').get("v.value"), 10);
@@ -48,6 +55,17 @@
             }
         }));
 
-    }
-    
+    },
+    cancelDialog: function(component, event, helper) {
+        var recId = component.get("v.recordId");
+        if (!recId) {
+            var homeEvt = $A.get("e.force:navigateToObjectHome");
+            homeEvt.setParams({
+                "scope": "Property__c"
+            });
+            homeEvt.fire();
+        } else {
+            helper.navigateTo(component, recId);
+        }
+    },    
 })
